42503038d834049d018544e3300e0265cd8c6415
EasierByCode[43] <daniel@community43.org>
2021-12-13 15:12:55 -0700

    update gh-pages

---
+++ src/routes/morningrecap.svelte
@@ -0,0 +1,16 @@
+<script context='module'>
+    export const load = async ({ fetch }) => {
+        const res = await fetch( 'morningrecap.json' );
+        const data = await res.json();
+
+        return {
+            props: {
+                data
+            }
+        }
+    }
+</script>
+
+<script type='ts'>
+    export let data;
+</script>
