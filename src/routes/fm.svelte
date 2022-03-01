4044be5efb5f3dd69b812b9d11ed0003f0bf6d86
EasierByCode[43] <daniel@community43.org>
2022-03-01 16:43:33 -0700

    add farmers market assets

--- src/routes/fm.svelte
+++ src/routes/fm.svelte
@@ -1,13 +1,311 @@
-<div class='row'>
-    <div class="col-6 col-md-4 col-three">
+<script>
+
+import base from "$lib/base";
+let granolaUrl = `${base}/fm-granola.jpg`;
+
+</script>
+<div class='row' style='margin-top: 33px'>
+    <div class="col-three">
+
         <div class='col-item'>
             <div class='product'>
-                <div class='image-box'></div>
+                <div class='image-box'>
+                    <a href="/products/lemon-basil">
+                        <span class="img" style="height: 105px; display: block;">        
+                            <img class="lazyload" style="height: 105px; display: block;" src={`${base}/fm-lemon-basil.jpg`} alt='lemon-basil'>
+                        </span>
+                        <span class="label-location"></span>
+                    </a>
+                </div>
                 <div class='description'>
                     <div class="row">
                         <ul class="add-info"><li><span class="text-uppercase">C43</span></li></ul>
                     </div>
-                    <h2 class="title">Granola</h2>
+                    
+                    <h2 class="title">
+                        <a href="/products/granola">Lemon Basil</a>
+                    </h2>
+
+                    <div class="price">
+                        <span>$6.00</span>
+                    </div>
+
+                    <div class="description" style="display:none;">Our signature loaf. It takes 3 days to make this bread using only organic flour, water, and sea salt. 100% naturally leavened.&nbsp;</div>
+
+                    <div class="product-inside-hover"><div class="row-btn">
+                        <form action="/cart/add" method="post" class="shopify-product-form" enctype="multipart/form-data">
+                          <input type="hidden" name="id" value="34087180468363">
+                          <input type="hidden" name="properties[Product type]" value="Non-perishable">
+                          
+                          <button class="btn-addtocart addtocart-item-js thumbprod-button-bg">
+                            
+                            
+                            <span class="icon icon-shopping_basket"></span> <span>ADD TO CART</span>
+                            
+                            
+                          </button>
+                          
+                        </form>
+                      </div><div class="row-btn">
+                        
+                
+                
+                      </div>
+                    </div>
+                </div>
+                <div class='actions'></div>
+            </div>
+        </div>
+
+        <div class='col-item'>
+            <div class='product'>
+                <div class='image-box'>
+                    <a href="/products/maple">
+                        <span class="img" style="height: 105px; display: block;">        
+                            <img class="lazyload" style="height: 105px; display: block;" src={`${base}/fm-maple.jpg`} alt='maple'>
+                        </span>
+                        <span class="label-location"></span>
+                    </a>
+                </div>
+                <div class='description'>
+                    <div class="row">
+                        <ul class="add-info"><li><span class="text-uppercase">C43</span></li></ul>
+                    </div>
+                    
+                    <h2 class="title">
+                        <a href="/products/granola">Maple Syrup</a>
+                    </h2>
+
+                    <div class="price">
+                        <span>$5.00</span>
+                    </div>
+
+                    <div class="description" style="display:none;">Our signature loaf. It takes 3 days to make this bread using only organic flour, water, and sea salt. 100% naturally leavened.&nbsp;</div>
+
+                    <div class="product-inside-hover"><div class="row-btn">
+                        <form action="/cart/add" method="post" class="shopify-product-form" enctype="multipart/form-data">
+                          <input type="hidden" name="id" value="34087180468363">
+                          <input type="hidden" name="properties[Product type]" value="Non-perishable">
+                          
+                          <button class="btn-addtocart addtocart-item-js thumbprod-button-bg">
+                            
+                            
+                            <span class="icon icon-shopping_basket"></span> <span>ADD TO CART</span>
+                            
+                            
+                          </button>
+                          
+                        </form>
+                      </div><div class="row-btn">
+                        
+                
+                
+                      </div>
+                    </div>
+                </div>
+                <div class='actions'></div>
+            </div>
+        </div>
+
+
+
+        <div class='col-item'>
+            <div class='product'>
+                <div class='image-box'>
+                    <a href="/products/granola">
+                        <span class="img" style="height: 105px; display: block;">        
+                            <img class="lazyload" style="height: 105px; display: block;" src={`${base}/fm-granola.jpg`} alt='granola'>
+                        </span>
+                        <span class="label-location"></span>
+                    </a>
+                </div>
+                <div class='description'>
+                    <div class="row">
+                        <ul class="add-info"><li><span class="text-uppercase">C43</span></li></ul>
+                    </div>
+                    
+                    <h2 class="title">
+                        <a href="/products/granola">Granola</a>
+                    </h2>
+
+                    <div class="price">
+                        <span>$9.00</span>
+                    </div>
+
+                    <div class="description" style="display:none;">Our signature loaf. It takes 3 days to make this bread using only organic flour, water, and sea salt. 100% naturally leavened.&nbsp;</div>
+
+                    <div class="product-inside-hover"><div class="row-btn">
+                        <form action="/cart/add" method="post" class="shopify-product-form" enctype="multipart/form-data">
+                          <input type="hidden" name="id" value="34087180468363">
+                          <input type="hidden" name="properties[Product type]" value="Non-perishable">
+                          
+                          <button class="btn-addtocart addtocart-item-js thumbprod-button-bg">
+                            
+                            
+                            <span class="icon icon-shopping_basket"></span> <span>ADD TO CART</span>
+                            
+                            
+                          </button>
+                          
+                        </form>
+                      </div><div class="row-btn">
+                        
+                
+                
+                      </div>
+                    </div>
+                </div>
+                <div class='actions'></div>
+            </div>
+        </div>
+
+        <div class='col-item'>
+            <div class='product'>
+                <div class='image-box'>
+                    <a href="/products/ranch">
+                        <span class="img" style="height: 105px; display: block;">        
+                            <img class="lazyload" style="height: 105px; display: block;" src={`${base}/fm-ranch.jpg`} alt='ranch'>
+                        </span>
+                        <span class="label-location"></span>
+                    </a>
+                </div>
+                <div class='description'>
+                    <div class="row">
+                        <ul class="add-info"><li><span class="text-uppercase">C43</span></li></ul>
+                    </div>
+                    
+                    <h2 class="title">
+                        <a href="/products/granola">Ranch</a>
+                    </h2>
+
+                    <div class="price">
+                        <span>$6.00</span>
+                    </div>
+
+                    <div class="description" style="display:none;">Our signature loaf. It takes 3 days to make this bread using only organic flour, water, and sea salt. 100% naturally leavened.&nbsp;</div>
+
+                    <div class="product-inside-hover"><div class="row-btn">
+                        <form action="/cart/add" method="post" class="shopify-product-form" enctype="multipart/form-data">
+                          <input type="hidden" name="id" value="34087180468363">
+                          <input type="hidden" name="properties[Product type]" value="Non-perishable">
+                          
+                          <button class="btn-addtocart addtocart-item-js thumbprod-button-bg">
+                            
+                            
+                            <span class="icon icon-shopping_basket"></span> <span>ADD TO CART</span>
+                            
+                            
+                          </button>
+                          
+                        </form>
+                      </div><div class="row-btn">
+                        
+                
+                
+                      </div>
+                    </div>
+                </div>
+                <div class='actions'></div>
+            </div>
+        </div>
+
+
+        <div class='col-item'>
+            <div class='product'>
+                <div class='image-box'>
+                    <a href="/products/strawberry">
+                        <span class="img" style="height: 105px; display: block;">        
+                            <img class="lazyload" style="height: 105px; display: block;" src={`${base}/fm-strawberry.jpg`} alt='strawberry'>
+                        </span>
+                        <span class="label-location"></span>
+                    </a>
+                </div>
+                <div class='description'>
+                    <div class="row">
+                        <ul class="add-info"><li><span class="text-uppercase">C43</span></li></ul>
+                    </div>
+                    
+                    <h2 class="title">
+                        <a href="/products/granola">Strawberry Syrup</a>
+                    </h2>
+
+                    <div class="price">
+                        <span>$5.00</span>
+                    </div>
+
+                    <div class="description" style="display:none;">Our signature loaf. It takes 3 days to make this bread using only organic flour, water, and sea salt. 100% naturally leavened.&nbsp;</div>
+
+                    <div class="product-inside-hover"><div class="row-btn">
+                        <form action="/cart/add" method="post" class="shopify-product-form" enctype="multipart/form-data">
+                          <input type="hidden" name="id" value="34087180468363">
+                          <input type="hidden" name="properties[Product type]" value="Non-perishable">
+                          
+                          <button class="btn-addtocart addtocart-item-js thumbprod-button-bg">
+                            
+                            
+                            <span class="icon icon-shopping_basket"></span> <span>ADD TO CART</span>
+                            
+                            
+                          </button>
+                          
+                        </form>
+                      </div><div class="row-btn">
+                        
+                
+                
+                      </div>
+                    </div>
+                </div>
+                <div class='actions'></div>
+            </div>
+        </div>
+
+
+        <div class='col-item'>
+            <div class='product'>
+                <div class='image-box'>
+                    <a href="/products/applesauce">
+                        <span class="img" style="height: 105px; display: block;">        
+                            <img class="lazyload" style="height: 105px; display: block;" src={`${base}/fm-applesauce.jpg`} alt='applesauce'>
+                        </span>
+                        <span class="label-location"></span>
+                    </a>
+                </div>
+                <div class='description'>
+                    <div class="row">
+                        <ul class="add-info"><li><span class="text-uppercase">C43</span></li></ul>
+                    </div>
+                    
+                    <h2 class="title">
+                        <a href="/products/granola">Applesauce</a>
+                    </h2>
+
+                    <div class="price">
+                        <span>$6.00</span>
+                    </div>
+
+                    <div class="description" style="display:none;">Our signature loaf. It takes 3 days to make this bread using only organic flour, water, and sea salt. 100% naturally leavened.&nbsp;</div>
+
+                    <div class="product-inside-hover"><div class="row-btn">
+                        <form action="/cart/add" method="post" class="shopify-product-form" enctype="multipart/form-data">
+                          <input type="hidden" name="id" value="34087180468363">
+                          <input type="hidden" name="properties[Product type]" value="Non-perishable">
+                          
+                          <button class="btn-addtocart addtocart-item-js thumbprod-button-bg">
+                            
+                            
+                            <span class="icon icon-shopping_basket"></span> <span>ADD TO CART</span>
+                            
+                            
+                          </button>
+                          
+                        </form>
+                      </div><div class="row-btn">
+                        
+                
+                
+                      </div>
+                    </div>
                 </div>
                 <div class='actions'></div>
             </div>
