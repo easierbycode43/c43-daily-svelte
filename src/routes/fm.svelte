5fa5ab28c43e5a5ab74492390815993c8f1ef655
EasierByCode[43] <daniel@community43.org>
2022-03-01 09:12:49 -0700

    start of farmers-market page

---
+++ src/routes/fm.svelte
@@ -0,0 +1,45 @@
+<div class='row'>
+    <div class="col-6 col-md-4 col-three">
+        <div class='col-item'>
+            <div class='product'>
+                <div class='image-box'></div>
+                <div class='description'>
+                    <div class="row">
+                        <ul class="add-info"><li><span class="text-uppercase">C43</span></li></ul>
+                    </div>
+                    <h2 class="title">Granola</h2>
+                </div>
+                <div class='actions'></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' />
+<style lang='scss'>
+    :global(body) {
+        background: url(https://thirsty.store/signup/bg_artistic.webp);
+        background-origin: border-box;
+        background-size: cover;
+    }
+
+    .col-three > .col-item {
+        flex: 0 0 33.33333%;
+        max-width: 33.33333%;
+    }
+
+    .add-info{
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .product .description .title {
+        font-family: 'Poppins', sans-serif;
+        font-size: 14px;
+    }
+</style>
